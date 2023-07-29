8b6b2913c5217d377489b4317708cdde7da2ec91
ykudale2012 <ykudale2012@gmail.com>
2023-07-29 10:51:03 +0530

    added rest api for cal

--- app.py
+++ app.py
@@ -6,9 +6,24 @@ app=Flask(__name__)
 def home():
     return 'Welcome to flask'
 
-@app.route('/cal',methods=['GET'])
+@app.route('/cal',methods=['GET','POST'])
 def math_operator():
-    return ''
+    operatation=request.json['operation']
+    number1=request.json['number1']
+    number2=request.json['number2']
+    result=0.0
+    if operatation=='add':
+        result=number1+number2
+    elif operatation=='sub':
+        result=number1-number2
+    elif operatation=='mul':
+        result=number1*number2
+    elif operatation=='div':
+        result=number1/number2
+    else:
+        return 'Invalid operartion type, it should be in (add,sub,mul,div)'
+
+    return f'result is: {result}'
 
 
 if __name__=='__main__':
